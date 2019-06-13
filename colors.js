0799af273beca1092d10dcb683eb3b39c555ab26
Madalin Gavanarescu <madalin@seomonitor.com>
2019-06-13 09:19:07 +0300

    Initial commit from gatsby: (https://github.com/madalingavanarescu/expect.git)

---
+++ colors.js
@@ -0,0 +1,26 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#f0e6f6',
+
+  /*primary: '#7c37ad',
+  primaryLight: '#ae66df',
+  primaryDark: '#4b007d',
+
+  secondary: '#ff4081',
+  secondaryLight: '#ff79b0',
+  secondaryDark: '#c60055',*/
+
+/*#009ACF
+C19AB7
+9C95DC
+0B7189
+170A1C*/
+
+  primary: '#009ACF',
+  primaryLight: '#5ecbff',
+  primaryDark: '#006c9e',
+
+  secondary: '#c19ab7',
+  secondaryLight: '#f4cbe9',
+  secondaryDark: '#906c87',
+};
